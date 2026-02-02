9f15b575a0b4679c3b2ea5fe0d52f6010c9953d8
Luka Vukovic <vukovicluka227@gmail.com>
2026-02-02 17:26:10 +0100

    register event route

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,8 +1,12 @@
 package server
 
 import (
+	"encoding/json"
+	"log"
+	"net"
 	"net/http"
 	"visitor/internal/hash"
+	"visitor/internal/model"
 	"visitor/internal/storage"
 )
 
@@ -16,12 +20,16 @@ type Server struct {
 func New(addr string, db *storage.DB, hasher *hash.Manager) *Server {
 	mux := http.NewServeMux()
 
-	return &Server{
+	s := &Server{
 		addr: 	addr,
 		db: 	db,
 		hasher: hasher,
 		mux: 	mux,
 	}
+
+	s.mux.HandleFunc("POST /api/event", s.handleEvent)
+
+	return s
 }
 
 func (s *Server) Start() error {
@@ -31,4 +39,46 @@ func (s *Server) Start() error {
 	}
 
 	return srv.ListenAndServe()
+}
+
+func (s *Server) handleEvent(w http.ResponseWriter, r *http.Request) {
+	var event model.EventRequest
+	if err := json.NewDecoder(r.Body).Decode(&event); err != nil {
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+
+	if event.Domain == "" {
+		http.Error(w, "Domain is required", http.StatusBadRequest)
+		return
+	}
+
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		ip = r.RemoteAddr
+	}
+
+	userAgent := r.Header.Get("User-Agent")
+
+	visitorHash, err := s.hasher.GetHash(r.Context(), event.Domain, ip, userAgent)
+	if err != nil {
+		log.Printf("failed to get visitor hash: %v", err)
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		return
+	}
+
+	pv := &model.PageView{
+		Domain: 		event.Domain,
+		Path: 			event.Path,
+		Referrer: 		event.Referrer,
+		VisitorHash: 	visitorHash,
+	}
+
+	if err := s.db.InsertPageView(r.Context(), pv); err != nil {
+		log.Printf("Failed to insert page view: %v", err)
+		http.Error(w, "internal error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
 }
